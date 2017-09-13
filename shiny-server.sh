a0254e3d4dd19a0bf8d1bb3689ab8d3abc0fec1a
Eun Woo Son <eunwoo.son@sdstate.edu>
2017-09-13 15:50:44 -0500

    sh script

--- shiny-server.sh
+++ shiny-server.sh
@@ -4,4 +4,7 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
+# Run rScript
+Rscript /usr/local/src/myscripts/librarySetup.R
+
 exec shiny-server >> /var/log/shiny-server.log 2>&1
