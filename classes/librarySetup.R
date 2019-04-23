5c614fc39c61bcb70cb427a8970c6c3604388b3d
andrew-leith <andrew-leith@users.noreply.github.com>
2019-04-23 16:50:23 -0400

    refactored librarySetup.R to conform to modern bioconductor syntax

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -16,9 +16,10 @@ list.of.bio.packages  <- c(
   "PREDAsampledata", "sfsmisc", "lokern", "multtest", "hgu133plus2.db", 
    "org.Ag.eg.db","org.At.tair.db","org.Bt.eg.db","org.Ce.eg.db","org.Cf.eg.db",
    "org.Dm.eg.db","org.EcK12.eg.db","org.EcSakai.eg.db","org.Gg.eg.db",
-   "org.Hs.eg.db","org.Hs.ipi.db","org.Mm.eg.db","org.Mmu.eg.db","org.Pf.plasmo.db",
+   "org.Hs.eg.db","org.Mm.eg.db","org.Mmu.eg.db","org.Pf.plasmo.db",
    "org.Pt.eg.db","org.Rn.eg.db","org.Sc.sgd.db","org.Ss.eg.db","org.Xl.eg.db"
 )
+# "org.Hs.ipi.db" removed because it is deprecated
 
  if(1) { # remove all old packages, to solve problem caused by Bioconductor upgrade
 	# create a list of all installed packages
@@ -41,14 +42,21 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 new.bio.packages <- list.of.bio.packages[!(list.of.bio.packages %in% installed.packages()[,"Package"])]
 notInstalledPackageCount = length(new.packages) + length(new.bio.packages)
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+    # install minet manually since it was recently moved from CRAN to bioconductor and breaks old packages that look for it in CRAN
+    BiocManager::install("minet", version = "3.8")
+
 #Install Require packages
 while(notInstalledPackageCount != 0){
 
-	if(length(new.packages)) install.packages(new.packages, repos="http://cran.rstudio.com/", dependencies=TRUE, quiet=TRUE)
-	if(length(new.bio.packages)){
-	  source("https://bioconductor.org/biocLite.R")
-	  biocLite(new.bio.packages, suppressUpdates = T, quiet=TRUE)
-	}
+        if(length(new.bio.packages)){
+          #source("https://bioconductor.org/biocLite.R")
+          #biocLite(new.bio.packages, suppressUpdates = T, quiet=TRUE)
+          BiocManager::install(new.bio.packages)
+        }
+        if(length(new.packages)) install.packages(new.packages, repos="http://cran.rstudio.com/", dependencies=TRUE, quiet=TRUE)
+
 
 	new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 	new.bio.packages <- list.of.bio.packages[!(list.of.bio.packages %in% installed.packages()[,"Package"])]
